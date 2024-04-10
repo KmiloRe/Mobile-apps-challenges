4ae0493eeb1cdacab25bb9045aec4c0447917a85
Camilo Restre <camilorestrerodriguez@gmail.com>
2024-04-10 16:32:12 -0500

    feat: even better colors

--- Java/java.java
+++ Java/java.java
@@ -6,20 +6,20 @@ public class java {
 
     public static String stringops(String str) {
         String rojo = "\u001B[41m";
-        String reset = "\u001B[0m"; 
+        String reset = "\u001B[0m";
         String amarillo = "\u001B[33m";
 
-        if (str == null || str.length() == 0 || str.isEmpty() ) {
+        if (str == null || str.length() == 0 || str.isEmpty()) {
             System.out.println(rojo + "No valido" + reset);
             return "no valido";
         }
-            //quito caracteres no ascii (stack)
+        // quito caracteres no ascii (stack)
         str.replaceAll("[^\\x00-\\x7F]", "");
 
         Map<Character, Integer> map = new HashMap<>();
         int inicio = 0;
         int maxLen = 0;
-        int inicioIndex = 0;
+        int inicioSofar = 0;
 
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
@@ -28,37 +28,33 @@ public class java {
             }
             if (i - inicio + 1 > maxLen) {
                 maxLen = i - inicio + 1;
-                inicioIndex = inicio;
+                inicioSofar = inicio;
             }
             map.put(ch, i);
         }
-        System.out.println("substring mas largo sin chars repetidos: " + amarillo + str.substring(inicioIndex, inicioIndex + maxLen) + reset);
-        return str.substring(inicioIndex, inicioIndex + maxLen);
+        System.out.println("substring mas largo sin chars repetidos: " + amarillo
+                + str.substring(inicioSofar, inicioSofar + maxLen) + reset);
+        return str.substring(inicioSofar, inicioSofar + maxLen);
     }
 
     public static void main(String[] args) {
-        //String input = "abcadcbsb$aiaosaftyuhjmnp$56a";
-        //String input = "sdasdsadsajaklsldkskoispia#$6tas";
-    //* para colores
-    String rojo = "\u001B[41m";
-    String reset = "\u001B[0m"; 
-    String amarillo = "\u001B[33m";
-    // Declaring the color 
-    // Custom declaration 
-    //public static final String ANSI_YELLOW = "\u001B[33m"; 
-  
-        
-        Scanner myObj = new Scanner(System.in); 
+        // todo: ver como poner color de fondo en el texto
+        String reset = "\u001B[0m";
+        String amarillo = "\u001B[33m";
 
-        
-        System.out.println(amarillo + "Enter username" + reset);
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println(amarillo + "Input" + reset + " ->");
 
         String input = myObj.nextLine();
         myObj.close();
-        String res = stringops(input);
-        //! esto deberia funcionar pero no lo hace
-        // // String aux=(res.equals("no valido") ? (amarillo + "no valido"+ reset) : res );
-        
-        //System.out.println("substring mas largo sin chars repetidos: " + amarillo + res + reset);
+        stringops(input);
+        // ! esto deberia funcionar pero no lo hace
+        // ? Como funciona el operador terciario en java???
+        // String aux=(res.equals("no valido") ? (amarillo + "no valido"+ reset) : res
+        // );
+
+        // //System.out.println("substring mas largo sin chars repetidos: " + amarillo +
+        // res + reset);
     }
 }
